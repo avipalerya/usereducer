4b953ce4f86ae0ec57d3589c7e9dadd62c0bedc6
avipalerya <avipaleriya27@gmail.com>
2022-09-12 10:29:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ComponentOne from "./components/ComponentOne";
+import DataFetching from "./components/DataFetching";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ComponentOne /> */}
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ComponentOne.js
@@ -0,0 +1,102 @@
+import React, { useReducer } from "react";
+const initialState = {
+  firstCounter: 0,
+  secondCounter: 0,
+};
+
+const reducer = (prevState, action) => {
+  console.log(prevState);
+  console.log(action);
+  switch (action.type) {
+    case "increment":
+      return {
+        ...prevState,
+        firstCounter: prevState.firstCounter + action.payload,
+      };
+    case "decrement":
+      return {
+        ...prevState,
+        firstCounter: prevState.firstCounter - action.payload,
+      };
+    case "reset":
+      return {
+        ...prevState,
+        firstCounter: initialState.firstCounter,
+      };
+
+    case "increment2":
+      return {
+        ...prevState,
+        secondCounter: prevState.secondCounter + action.payload,
+      };
+    case "decrement2":
+      return {
+        ...prevState,
+        secondCounter: prevState.secondCounter - action.payload,
+      };
+    case "reset2":
+      return {
+        ...prevState,
+        secondCounter: initialState.secondCounter,
+      };
+    default:
+      return prevState;
+  }
+};
+const ComponentOne = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      <div>
+        <h1>First Count-{state.firstCounter}</h1>
+        <button
+          onClick={() => {
+            dispatch({ type: "increment", payload: 5 });
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "decrement", payload: 5 });
+          }}
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "reset" });
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        <h1>Secound Count-{state.secondCounter}</h1>
+        <button
+          onClick={() => {
+            dispatch({ type: "increment2", payload: 10 });
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "decrement2", payload: 10 });
+          }}
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "reset2" });
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ComponentOne;
